577c1450d16a445c7fbfde0c2c5e3e9f2c2f2efc
vasin limsukhawat <vasin.limsukhawat@gmail.com>
2015-12-14 23:41:31 -0700

    add jomself section

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -6,6 +6,16 @@
     .controller('MainController', MainController);
 
   /** @ngInject */
-  function MainController() {
+  function MainController($scope) {
+  	$scope.animateElementIn = function($el) {
+    $el.removeClass('hidden');
+    $el.addClass('animated shake'); // this example leverages animate.css classes
+  };
+
+  $scope.animateElementOut = function($el) {
+    $el.addClass('hidden');
+    $el.removeClass('animated shake'); // this example leverages animate.css classes
+  };
+ 
   }
 })();
