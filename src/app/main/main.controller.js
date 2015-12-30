f0150676d95d16882c9b38a757348bca5ee3b65c
vasin limsukhawat <vasin.limsukhawat@gmail.com>
2015-12-29 20:11:58 -0700

    custom navbar

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -7,13 +7,5 @@
 
   /** @ngInject */
   function MainController($state, $scope) {
-    var vm = this;
-  	vm.goToThankyou = function() {
-      console.log('gogogo');
-      $state.go('thankyou');
-    };
-
-  
- 
   }
 })();
